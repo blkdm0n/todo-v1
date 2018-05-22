1c5272e79cf20eb2977288a55f1ebe6249a8ba97
Bernard Kitchens <bernard.kitchens@gmail.com>
2018-05-21 17:00:22 -0700

    requirement 2 complete

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //REQ #1 - It should have a place to store todos
 
-var todos = [`item1`, `item2`, `item3`];
+var todos = [`study algorithms`, `white boarding practice`, `get a job`];
 
 //REQ #2 - It should have a way to display todos
 
+console.log(`My Todos`, todos)
+
+//REQ #3 - It should have a way 
